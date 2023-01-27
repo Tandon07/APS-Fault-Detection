ebeedd68e00cdf6d85f01683df3abbf8a433457e
Tandon07 <tandonsaurabh07@gmail.com>
2023-01-28 01:10:28 +0530

    updatteedd

--- airflow/dags/batch_prediction..py
+++ airflow/dags/batch_prediction..py
@@ -19,25 +19,42 @@ with DAG(
 ) as dag:
 
     
-    def training(**kwargs):
-        from sensor.pipeline.training_pipeline import start_training_pipeline
-        start_training_pipeline()
-    
-    def sync_artifact_to_s3_bucket(**kwargs):
+    def download_files(**kwargs):
         bucket_name = os.getenv("BUCKET_NAME")
-        os.system(f"aws s3 sync /app/artifact s3://{bucket_name}/artifacts")
-        os.system(f"aws s3 sync /app/saved_models s3://{bucket_name}/saved_models")
+        input_dir = "/app/input_files"
+        #creating directory
+        os.makedirs(input_dir,exist_ok=True)
+        os.system(f"aws s3 sync s3://{bucket_name}/input_files /app/input_files")
 
-    training_pipeline  = PythonOperator(
-            task_id="train_pipeline",
-            python_callable=training
+    def batch_prediction(**kwargs):
+        from sensor.pipeline.batch_prediction import start_batch_prediction
+        input_dir = "/app/input_files"
+        for file_name in os.listdir(input_dir):
+            #make prediction
+            start_batch_prediction(input_file_path=os.path.join(input_dir,file_name))
+    
+    def sync_prediction_dir_to_s3_bucket(**kwargs):
+        bucket_name = os.getenv("BUCKET_NAME")
+        #upload prediction folder to predictionfiles folder in s3 bucket
+        os.system(f"aws s3 sync /app/prediction s3://{bucket_name}/prediction_files")
+    
+
+    download_input_files  = PythonOperator(
+            task_id="download_file",
+            python_callable=download_files
 
     )
 
-    sync_data_to_s3 = PythonOperator(
-            task_id="sync_data_to_s3",
-            python_callable=sync_artifact_to_s3_bucket
+    generate_prediction_files = PythonOperator(
+            task_id="prediction",
+            python_callable=batch_prediction
 
     )
 
-    training_pipeline >> sync_data_to_s3    
+    upload_prediction_files = PythonOperator(
+            task_id="upload_prediction_files",
+            python_callable=sync_prediction_dir_to_s3_bucket
+
+    )
+
+    download_input_files >> generate_prediction_files >> upload_prediction_files
